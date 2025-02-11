0fec8991dfb40b08b4cec088f5530eb09a5da74a
aveaustin <120065814+aveaustin@users.noreply.github.com>
2025-02-11 07:10:07 +0100

    Experimenting with goal verification for party members

--- lib/screens/goals/my_goals_screen.dart
+++ lib/screens/goals/my_goals_screen.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class MyGoalsScreen extends StatefulWidget {
   const MyGoalsScreen({super.key});
 
-//TODO: Public API use for individual goals is likely a no-no at scale
   @override
   MyGoalsScreenState createState() => MyGoalsScreenState();
 }
@@ -95,11 +94,110 @@ class MyGoalsScreenState extends State<MyGoalsScreen> {
         'goal': _goalController.text,
         'details': _detailsController.text,
         'frequency': _frequency,
+        'completedDays': [],
+        'verificationRequests':
+            {}, // Start with an empty map for verification requests
         'timestamp': FieldValue.serverTimestamp(),
+        'creatorId': user.uid, // Store the user ID who created the goal
+        'verifiedDays': [],
       });
       _goalController.clear();
       _detailsController.clear();
       _frequency = 1;
+    }
+  }
+
+  // This method will be used to request verification for a specific day
+  void _requestVerification(String goalId, String day) async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      // Get the current verification requests
+      DocumentSnapshot goalDoc = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('goals')
+          .doc(goalId)
+          .get();
+
+      if (goalDoc.exists) {
+        final goalData = goalDoc.data() as Map<String, dynamic>;
+        final verificationRequests = goalData['verificationRequests'] ?? {};
+
+        // Only change the selected day's status to true (yellow question mark)
+        if (verificationRequests[day] != true) {
+          await _firestore
+              .collection('users')
+              .doc(user.uid)
+              .collection('goals')
+              .doc(goalId)
+              .update({
+            'verificationRequests': {
+              ...verificationRequests,
+              day: true, // Request verification for the day
+            }
+          });
+        }
+      }
+    }
+  }
+
+  Future<void> _verifyGoal(String goalId, String day) async {
+    User? user = _auth.currentUser;
+    final goalDoc = await _firestore
+        .collection('users')
+        .doc(user!.uid)
+        .collection('goals')
+        .doc(goalId)
+        .get();
+
+    if (goalDoc.exists) {
+      final goalData = goalDoc.data()!;
+      final creatorId = goalData['creatorId'];
+
+      // Get party members
+      DocumentSnapshot partyDoc = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('party')
+          .doc('partyDetails')
+          .get();
+
+      final partyMembers = List<String>.from(partyDoc['members'] ?? []);
+
+      // Only allow party members to verify each other's goals
+      if (partyMembers.contains(user.uid) && creatorId != user.uid) {
+        // If user is not the creator and is in the party, verify the goal
+        await _firestore
+            .collection('users')
+            .doc(user.uid)
+            .collection('goals')
+            .doc(goalId)
+            .update({
+          'verificationRequests': {
+            day: false, // Mark the request as verified by setting it to false
+          },
+          'verifiedDays':
+              FieldValue.arrayUnion([day]), // Add day to verifiedDays
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content:
+                  Text('You can only verify goals of other party members.')),
+        );
+      }
+    }
+  }
+
+  Future<void> _deleteGoal(String goalId) async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('goals')
+          .doc(goalId)
+          .delete();
     }
   }
 
@@ -142,6 +240,13 @@ class MyGoalsScreenState extends State<MyGoalsScreen> {
                     itemBuilder: (context, index) {
                       final goalData =
                           goals[index].data() as Map<String, dynamic>;
+                      final List<dynamic> completedDays =
+                          goalData['completedDays'] ?? [];
+                      final int remaining =
+                          goalData['frequency'] - completedDays.length;
+                      final verificationRequests =
+                          goalData['verificationRequests'] ?? {};
+                      final verifiedDays = goalData['verifiedDays'] ?? [];
                       return Card(
                         margin: const EdgeInsets.symmetric(vertical: 5),
                         child: ListTile(
@@ -153,17 +258,54 @@ class MyGoalsScreenState extends State<MyGoalsScreen> {
                                 Text(goalData['details']),
                               Text(
                                   'Frequency: ${goalData['frequency']} times/week'),
+                              Text('Completed: ${completedDays.length} times'),
+                              Text('Remaining: $remaining times'),
+                              const SizedBox(height: 10),
+                              for (var day in [
+                                'Sunday',
+                                'Monday',
+                                'Tuesday',
+                                'Wednesday',
+                                'Thursday',
+                                'Friday',
+                                'Saturday'
+                              ])
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(day),
+                                    IconButton(
+                                      icon: Icon(
+                                        verificationRequests[day] == true
+                                            ? Icons.help_outline
+                                            : Icons.circle_outlined,
+                                        color: verificationRequests[day] == true
+                                            ? Colors.orange
+                                            : Colors.grey,
+                                      ),
+                                      onPressed: () {
+                                        if (verificationRequests[day] == true) {
+                                          // Verify goal if the user is not the creator
+                                          _verifyGoal(goals[index].id, day);
+                                        } else {
+                                          // Request verification for the day
+                                          _requestVerification(
+                                              goals[index].id, day);
+                                        }
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              if (verifiedDays.isNotEmpty)
+                                Text(
+                                    'Verified Days: ${verifiedDays.join(', ')}'),
                             ],
                           ),
                           trailing: IconButton(
                             icon: const Icon(Icons.delete, color: Colors.red),
                             onPressed: () async {
-                              await _firestore
-                                  .collection('users')
-                                  .doc(_auth.currentUser!.uid)
-                                  .collection('goals')
-                                  .doc(goals[index].id)
-                                  .delete();
+                              await _deleteGoal(goals[index].id);
                             },
                           ),
                         ),
--- lib/screens/party/party_screen.dart
+++ lib/screens/party/party_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PartyScreen extends StatefulWidget {
   const PartyScreen({super.key});
@@ -8,18 +10,60 @@ class PartyScreen extends StatefulWidget {
 }
 
 class _PartyScreenState extends State<PartyScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _inviteController = TextEditingController();
-  List<String> _members = ["User1", "User2", "User3"]; // Temporary mock data
-  List<String> _pendingInvites = ["PendingUser1", "PendingUser2"]; // Mock data
+  List<String> _members = []; // Will be fetched from Firestore
+  List<String> _pendingInvites = []; // Will be fetched from Firestore
 
+  // Fetch current party members and pending invites
+  Future<void> _fetchPartyData() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      // Fetch party members
+      DocumentSnapshot partyDoc = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('party')
+          .doc('partyDetails')
+          .get();
+
+      if (partyDoc.exists) {
+        setState(() {
+          _members = List<String>.from(partyDoc['members'] ?? []);
+          _pendingInvites = List<String>.from(partyDoc['pendingInvites'] ?? []);
+        });
+      }
+    }
+  }
+
+  // Send an invite to a user
   void _sendInvite() {
     String invitee = _inviteController.text.trim();
     if (invitee.isNotEmpty) {
-      setState(() {
-        _pendingInvites.add(invitee);
-      });
-      _inviteController.clear();
+      User? user = _auth.currentUser;
+      if (user != null) {
+        _firestore
+            .collection('users')
+            .doc(user.uid)
+            .collection('party')
+            .doc('partyDetails')
+            .update({
+          'pendingInvites': FieldValue.arrayUnion([invitee]),
+        });
+
+        setState(() {
+          _pendingInvites.add(invitee);
+        });
+        _inviteController.clear();
+      }
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPartyData();
   }
 
   @override
